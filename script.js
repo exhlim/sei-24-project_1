c317a6ec60a3430c6ae29acb5102f92f2ee71293
exhlim <exhlimm@gmail.com>
2020-08-17 00:56:40 +0800

    Added design for card front Added flip animations for the card Added Start, victory and gameOver overlay screens Added dancing animations to icons Added click function to all overlay screens

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-// Cloning my card back
+// Generate Cards first thing when the page loads
 var cardsContainer = document.querySelector(".cardsContainer");
-for(let i = 0; i < 17; i++){
+for(let i = 0; i < 15; i++) {
+    // Cloning the first entire card div
     var cloneArr = document.querySelector(".card").cloneNode(true);
+    // Accessing the .icon class which is the image class
+    var iconClass = document.querySelector(".icon");
+    // Change the image base on the loop condition
+    iconClass.src = "img/ai_logo/" + ((i + 1) % 8)+ ".png";
     cardsContainer.appendChild(cloneArr);
 }
-// Cloning my card front
+
+// Game Variables
+var overlays = document.querySelectorAll(".overlay");
+var cards = document.querySelectorAll(".card");
+var cardFront = document.querySelector(".cardFront");
+
+// Game class
+class gameStatus {
+    constructor(time, card) {
+
+    }
+}
 
 
 
-
+// Add click event to all my overlays
+overlays.forEach(overlay => {
+    overlay.addEventListener("click", () => {
+        overlay.classList.remove("visible");
+        // initializegame();
+    })
+});
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.add("visible");
+    })
+});
 
 
 
