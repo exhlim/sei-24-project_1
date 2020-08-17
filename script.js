00bd30c1de91d50b96628460ac5000d2c8015307
exhlim <exhlimm@gmail.com>
2020-08-17 18:07:02 +0800

    Added overall card matching functions Added function to update rating and timer

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for(let i = 0; i < 15; i++) {
     var cloneArr = document.querySelector(".card").cloneNode(true);
     // Accessing the .icon class which is the image class
     var iconClass = document.querySelector(".icon");
+    cardsContainer.setAttribute("style", "grid-template-columns: repeat(4, auto);")
     // Change the image base on the loop condition
     iconClass.src = "img/ai_logo/" + ((i + 1) % 8)+ ".png";
     cardsContainer.appendChild(cloneArr);
@@ -14,6 +15,7 @@ for(let i = 0; i < 15; i++) {
 var overlays = document.querySelectorAll(".overlay");
 var cards = document.querySelectorAll(".card");
 var cardFront = document.querySelector(".cardFront");
+var display;
 
 // Game class
 class gameStatus {
@@ -36,17 +38,67 @@ class gameStatus {
         // Reset matchedCards array back to 0 and shuffle my cards again
         this.matchedCards = [];
         this.shuffleCards();
+        this.cardToBeChecked = null;
+        // Start the count down timer
+        this.startCountDown();
+        this.busy = false;
     }
-    flipCard(card) {
-        if(this.canFlipCard(card)){
+
+    // CARD RELATED FUNCTIONS
+    flipCard(cardJustClicked) {
+        if(this.canFlipCard(cardJustClicked)){
+            // Increase move count
             this.moves.textContent++;
+            // Check the rating based on the moves
             this.ratingCheck(parseInt(this.moves.textContent));
-            card.classList.add("visible");
+            // Makee the card visible
+            cardJustClicked.classList.add("visible");
+
+            if(this.cardToBeChecked) {
+                this.checkForCardMatch(cardJustClicked)
+            }
+            else {
+                this.cardToBeChecked = cardJustClicked;
+            }
         }
     }
     canFlipCard(card) {
-        return true;
+        return (!this.busy && card !== this.cardToBeChecked && !this.matchedCards.includes(card));
     }
+    checkForCardMatch(cardJustClicked) {
+        if(this.recognise(cardJustClicked) === this.recognise(this.cardToBeChecked)){
+            // add the cardJustClicked and cardToBeChecked to the matched array
+            this.matchedCards.push(cardJustClicked);
+            this.matchedCards.push(this.cardToBeChecked);
+            // add the matched class to the cards
+            cardJustClicked.classList.add("matched");
+            this.cardToBeChecked.classList.add("matched");
+            // reset the cardToBeChecked back to null
+            this.cardToBeChecked = null;
+            if(this.matchedCards.length === this.cardsArray.length){
+                this.victory();
+            }
+        }
+        else {
+            this.busy = true;
+            //When the card is not matched.
+            setTimeout(() =>{
+                cardJustClicked.classList.remove("visible");
+                this.cardToBeChecked.classList.remove("visible");
+                this.cardToBeChecked = null;
+                this.busy = false;
+            }, 800);
+
+        }
+    }
+    recognise(card) {
+        display = card.getElementsByClassName("icon")["id"].src;
+        // console.log(display);
+        // console.log((display.match(/[0-9]/gi)|| [])[3]);
+        return display;
+    }
+
+
     shuffleCards(){
         // Looping through the entire array starting from the back
         // RandomIndex is generated from 0 to 15, .length -1 so that you dont touch the index 16 fisheryates shuffle
@@ -57,14 +109,6 @@ class gameStatus {
             this.cardsArray[i].style.order = randomIndex;
         }
     }
-    ratingCheck(currentMoves) {
-    if(currentMoves > 11 && currentMoves < 16)
-        this.ratings.textContent = "★ ★ ☆";
-    else if(currentMoves >= 16)
-        this.ratings.textContent = "★ ☆ ☆";
-    else
-        this.ratings.textContent = "★ ★ ★";
-    }
     resetCards() {
         this.cardsArray.forEach(card => {
             card.style.display = "none";
@@ -74,6 +118,36 @@ class gameStatus {
         //     setTimeout(() => {
         //          }, 0);
         })
+    }
+
+
+    // RATING FUNCTIONS
+    ratingCheck(currentMoves) {
+    if(currentMoves > 11 && currentMoves < 16)
+        this.ratings.textContent = "★ ★ ☆";
+    else if(currentMoves >= 16)
+        this.ratings.textContent = "★ ☆ ☆";
+    else
+        this.ratings.textContent = "★ ★ ★";
+    }
+
+    // TIMING FUNCTIONS
+    startCountDown() {
+        this.countDownRef = setInterval(() => {
+            this.timer.innerText--;
+            if(this.timer.innerText == "0")
+                this.gameOver();
+        }, 1000);
+    }
+
+    // GAME OVER FUNCTIONS
+    gameOver() {
+        clearInterval(this.countDownRef);
+        document.getElementById("gameOver").classList.add("visible");
+    }
+    victory() {
+        clearInterval(this.countDownRef);
+        document.getElementById("victory").classList.add("visible");
     }
 }
 let newGame = new gameStatus(100, cards)
@@ -102,4 +176,3 @@ cards.forEach(card => {
 
 
 //for harder difficulty
-// cardsContainer.setAttribute("style", "grid-template-columns: repeat(8, auto);")
